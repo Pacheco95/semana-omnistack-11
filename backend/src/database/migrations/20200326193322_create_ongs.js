3e56f093671c283b9f97331a520a661e4177bb2c
Michael Pacheco <mdpgd95@gmail.com>
2020-04-04 16:34:10 -0300

    add password column to ongs table

--- backend/src/database/migrations/20200326193322_create_ongs.js
+++ backend/src/database/migrations/20200326193322_create_ongs.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     table.string('id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable();
+    table.string('password', 80).notNullable().defaultTo('');
     table.string('whatsapp').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
